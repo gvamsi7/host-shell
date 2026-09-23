5bb387f0893f9c948445c87ffb82aaf82cfec862
Vamshi Gorre <gvamshi906@gmail.com>
2026-09-24 01:48:10 +0530

    Serve remote offer bundles through host port

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,8 +2,21 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const paymentsDist =
+  process.env.OFFER_PAYMENTS_DIST ||
+  path.resolve(__dirname, '..', 'offer-payments', 'dist')
+
+const tradingDist =
+  process.env.OFFER_TRADING_DIST ||
+  path.resolve(__dirname, '..', 'offer-trading', 'dist')
+
+const analyticsDist =
+  process.env.OFFER_ANALYTICS_DIST ||
+  path.resolve(__dirname, '..', 'offer-analytics', 'dist')
+
 module.exports = {
   entry: path.resolve(__dirname, 'src/main.jsx'),
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'shell.js',
@@ -11,9 +24,11 @@ module.exports = {
     clean: true,
     iife: true,
   },
+
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
   },
+
   module: {
     rules: [
       {
@@ -31,6 +46,7 @@ module.exports = {
       },
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'index.html'),
@@ -45,21 +61,40 @@ module.exports = {
       ],
     }),
   ],
+
   optimization: {
     splitChunks: false,
     runtimeChunk: false,
   },
+
   devtool: false,
+
   devServer: {
     host: '0.0.0.0',
     port: 3000,
     historyApiFallback: true,
     hot: false,
     liveReload: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+
+    static: [
+      {
+        directory: paymentsDist,
+        publicPath: '/offers/payments',
+        watch: true,
+      },
+      {
+        directory: tradingDist,
+        publicPath: '/offers/trading',
+        watch: true,
+      },
+      {
+        directory: analyticsDist,
+        publicPath: '/offers/analytics',
+        watch: true,
+      },
+    ],
   },
+
   performance: {
     hints: false,
   },
